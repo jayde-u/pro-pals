576909132d0840087864ae43d6568b2a84e016ea
jiwon-u <jayde0yu@gmail.com>
2025-06-14 17:15:10 +0900

    feat: implement static SearchBox component and styling

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,9 +1,18 @@
+import styled from "styled-components";
+
+const Search = styled.input`
+    border: 1px solid #eee;
+    border-radius: 5px;
+    width: 20%;
+    margin: 5px;
+`;
 
 function SearchBox() {
     return (
-            <>
-            </>
-        );
+        <>
+            <Search type="text" placeholder="search..."/>
+        </>
+    );
 }
 
 export default SearchBox;
