be8ed94d8f958d5f8cf811f3653aeae549100fba
jiwon-u <jayde0yu@gmail.com>
2025-06-20 19:21:59 +0900

    style: create GlobalStyle with base styles

---
+++ src/GlobalStyle.jsx
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  html, body {
+    height: 100%;
+    background-color: rgb(255, 255, 255);
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+export default GlobalStyle;
