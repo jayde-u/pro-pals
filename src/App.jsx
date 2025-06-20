7dffcac5584b13ea7d1c365594bd20538d790bc3
jiwon-u <jayde0yu@gmail.com>
2025-06-20 19:20:37 +0900

    style: apply 'Plaster' Google Font to Title

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import GlobalStyle from './GlobalStyle';
 import SearchBox from './components/SearchBox';
 import CardList from './components/CardList';
 import './App.css'
+
+const Title = styled.h1`
+  text-align: center;
+  width: 100vw;
+  font-family: "Plaster", sans-serif;
+  font-weight: 400;
+  font-size: 4em;
+`
 
 function App() {
   const [searchText, setSearchText] = useState("");
   const [pals, setPals] = useState([]);
   useEffect(() => {
-    fetch("https://randomuser.me/api/?results=30&inc=id,name,email&format=json&noinfo")
+    fetch("https://randomuser.me/api/?results=10&inc=id,name,email&format=json&noinfo")
       .then((res) => res.json())  
       .then((data) => {
           const loadedPals = data.results.map((person) => ({
@@ -22,7 +32,8 @@ function App() {
 
   return (
     <>
-      <h1>Pro Pals</h1>
+      <GlobalStyle />
+      <Title>PRO PALS</Title>
       <SearchBox 
           searchText={searchText} 
           onSearchTextChange={setSearchText} />
