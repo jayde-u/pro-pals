2664fdd7bf47ab9f0df2fdd8494154cce078b0a9
jiwon-u <jayde0yu@gmail.com>
2025-06-20 19:23:09 +0900

    style: update SearchBox layout and styling

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,21 +1,29 @@
 import styled from "styled-components";
 
+const SearchContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    margin-top: 10px;
+    margin-bottom: 10px;
+`
 const Search = styled.input`
-    border: 1px solid #eee;
-    border-radius: 5px;
-    width: 20%;
+    border: 1px solid #cdcdcd;
+    border-radius: 3px;
+    width: 90vw;
+    max-width: 500px;
     margin: 5px;
+    font-size: 1.2em;
 `;
 
 function SearchBox({ searchText, onSearchTextChange }) {
     return (
-        <>
+        <SearchContainer>
             <Search 
                 type="text" 
                 placeholder="search..."
                 value={searchText}
                 onChange={(e) => onSearchTextChange(e.target.value)} />
-        </>
+        </SearchContainer>
     );
 }
 
